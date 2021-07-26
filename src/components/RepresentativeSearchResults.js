48a047d29792871dc54395e1e4a3404204bac4a4
luizsilva <luizfsilva.091595@gmail.com>
2021-07-26 15:15:14 -0400

    added office title, representative name and phone number to results

--- src/components/RepresentativeSearchResults.js
+++ src/components/RepresentativeSearchResults.js
@@ -1,14 +1,22 @@
 function RepresentativeSearchResults(props) {
+  const { offices, officials } = props
 
-    const {offices, officials} =props;
-
-    return (
-        <div>
-            {offices.map(office => 
-                <div>{office.name}</div>
-            )}
-        </div>
-    )
+  return (
+    <div>
+      {offices.map(office => {
+        const official = officials[office.officialIndices[0]]
+        return (
+          <div className="representative-info">
+            <div className="office-title">{office.name}</div>
+            <p>{official?.name}</p>
+            <label>
+              Phone:&nbsp;<span>{official?.phones[0]}</span>
+            </label>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default RepresentativeSearchResults
