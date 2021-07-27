38adeda69671a7f39d05b36d61d8afec13874f93
luizsilva <luizfsilva.091595@gmail.com>
2021-07-27 11:03:01 -0400

    created a sign in link

---
+++ src/components/SignIn.js
@@ -0,0 +1,47 @@
+import {useState} from 'react'
+
+function SignIn() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const signInUser = (event) => {
+        event.preventDefault()
+        console.log("signing in...")
+    }
+
+    return (
+        <div className="sign-up-container">
+          <form onSubmit={(event) => signInUser(event)}>
+              <label className="form-label">
+                  <strong>Email:&nbsp;</strong>
+                  <input 
+                    name="email"
+                    type="text"
+                    className="form-input"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                  />
+              </label>
+              <br/>
+              <label className="form-label">
+                  <strong>Password:&nbsp;</strong>
+                  <input 
+                    name="password"
+                    type="password"
+                    className="form-input"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                  />
+              </label>
+              <button
+                className="submit-btn"
+                type="submit"
+              >
+                  SUBMIT
+              </button>
+          </form>
+        </div>
+    )
+}
+
+export default SignIn
